dd4af30c32ee0ef3656dfe6bc3e08067818ea253
HsupoLeng <hsupo.leng@gmail.com>
2019-06-01 14:30:17 -0700

    Add predict w. firing map; Fix map nan bug; Add T-region
    Execute run_predict_location_with_place_cell_firing_map.m to check the
    result.
    
    This commit implements prediction using weighted sum of firing rate maps. Prediction is done using spikes during ripples in the T-region (decision region). See place_cell_choice.m for definition of T-region, coded 7. Only experimented with outbound trials.
    
    Fix bug in place_cell_spatial_firing_rate.m so now the firing rate map
    contains no nan.
    
    For convenience, commonly used scripts are converted to functions.

---
+++ predict_location_with_place_cell_firing_map.m
@@ -0,0 +1,101 @@
+function [accuracy, samples] = predict_location_with_place_cell_firing_map(day, epoch, start_choice_idx)
+    data_dir='../dataset/Bon/';
+    name='bon';
+    %day=4;
+    %epoch=4;
+    [pos_t,pos_p,pos_v,sp_all]=load_data(data_dir,name,day,epoch);
+
+    load(sprintf('../results/%schoice%d-%d.mat', name, day, epoch), 'choice');
+    load(sprintf('../results/spikes_in_ripple_all-day_%d-epoch_%d.mat', day, epoch), 'spikes_in_ripple_all');
+    load(sprintf('../results/spatial_firing_rate_by_unit-day_%d-epoch_%d.mat', day, epoch), 'spatial_firing_rate_by_unit', 'p_min');
+
+    % Convert time unit in choice to seconds
+    for i=1:size(choice, 1)
+        choice(i, 2) = pos_t(choice(i, 2));
+        choice(i, 3) = pos_t(choice(i, 3));
+    end
+
+    % Uncomment this if you want to predict on part of epoch
+    choice = choice(start_choice_idx:end, :);
+    %% Predict decision in outbound or inbound trials
+    regions = [-1, 0, 1];
+    predict_opt_str = 'outbound';
+    xleft=round(55-p_min(1))+1;    % x<55 is left
+    xright=round(90-p_min(1))+1;   % x>90 is right
+    ycheck=round(110-p_min(2))+1; % check region if y>110
+
+    if strcmp(predict_opt_str, 'outbound')
+        trial_start = 0;
+        predictor_small = @(fr_map) sum(sum(fr_map(1:xleft, :)));
+        predictor_large = @(fr_map) sum(sum(fr_map(xright:end, :)));
+    elseif strcmp(predict_opt_str, 'inbound_from_left')
+        trial_start = -1;
+        predictor_small = @(fr_map) sum(sum(fr_map(xleft:xright, :)));
+        predictor_large = @(fr_map) sum(sum(fr_map(xright:end, :)));
+    elseif strcmp(predict_opt_str, 'inbound_from_right')
+        trial_start = 1;
+        predictor_small = @(fr_map) sum(sum(fr_map(1:xleft, :)));
+        predictor_large = @(fr_map) sum(sum(fr_map(xleft:xright, :)));
+    else % default to outbound
+        trial_start = 0;
+        predictor_small = @(fr_map) sum(sum(fr_map(1:xleft, :)));
+        predictor_large = @(fr_map) sum(sum(fr_map(xright:end, :)));
+    end
+    candidates = regions(regions~=trial_start);
+
+    num_samples = 1000;
+    t_period_abs_idxs = find(choice(:,1)==7);
+    t_idxs_to_remove = [];
+    for i=1:length(t_period_abs_idxs)
+        prev_region_idx = find(choice(1:t_period_abs_idxs(i), 1) ~= 7, 1, 'last');
+        if choice(prev_region_idx, 1) ~= trial_start
+            t_idxs_to_remove = [t_idxs_to_remove, i];
+        end
+    end
+    t_period_abs_idxs(t_idxs_to_remove) = [];
+    t_boundaries = choice(t_period_abs_idxs, 2:3);
+    t_periods = cellfun(@(boundary) boundary(1):(1/30):boundary(2), num2cell(t_boundaries, 2), 'UniformOutput', false);
+    t_periods_all = horzcat(t_periods{:});
+
+    samples = struct('timestamp', nan, 'prev_region', nan, 'next_region', nan, 'pred_region', nan, 't_abs_idx', nan, 'prev_region_abs_idx', nan);
+    rand_timestamps = randi(length(t_periods_all), num_samples, 1);
+    for i=1:length(rand_timestamps)
+        samples(i).timestamp = t_periods_all(rand_timestamps(i));
+        t_idx = find(cellfun(@(period) any(ismember(samples(i).timestamp, period)), t_periods)); 
+        t_abs_idx = t_period_abs_idxs(t_idx);
+        samples(i).t_abs_idx = t_abs_idx;
+        prev_region_idx = find(choice(1:t_abs_idx, 1) ~= 7, 1, 'last');
+        samples(i).prev_region = choice(prev_region_idx, 1);
+        samples(i).prev_region_abs_idx = prev_region_idx;
+        next_region_idx = find(choice(t_abs_idx:end, 1) ~= 7, 1, 'first');
+        samples(i).next_region = choice(t_abs_idx + next_region_idx -1, 1);    
+    end 
+
+    for i=1:length(samples)
+        prev_spike_mask = bitand([spikes_in_ripple_all(:).start_sec] < samples(i).timestamp, ...
+            [spikes_in_ripple_all(:).start_sec] > choice(samples(i).t_abs_idx, 2));
+        if ~any(prev_spike_mask)
+            samples(i).pred_region = nan;
+            continue; 
+        else
+            spiking_neuron_mat = [spikes_in_ripple_all(prev_spike_mask).tetrode; spikes_in_ripple_all(prev_spike_mask).neuron]';
+            [spiking_neuron_unique_mat, ~, neuron_idxs] = unique(spiking_neuron_mat, 'rows');
+            num_spikes = histcounts(neuron_idxs, 'BinMethod', 'integers');
+            weight_factor = num_spikes./sum(num_spikes);
+            weighted_map = zeros(size(spatial_firing_rate_by_unit(1).firing_rate_map));
+            for j=1:length(weight_factor)
+                idx = find(bitand([spatial_firing_rate_by_unit(:).tetrode]==spiking_neuron_unique_mat(j, 1), ...
+                    [spatial_firing_rate_by_unit(:).neuron]==spiking_neuron_unique_mat(j, 2)));
+                weighted_map = weighted_map + weight_factor(j).*spatial_firing_rate_by_unit(idx).firing_rate_map;
+            end
+            [~, pred_idx] = max([predictor_small(weighted_map), predictor_large(weighted_map)]);
+            samples(i).pred_region = candidates(pred_idx);
+    %         figure(1);
+    %         imagesc(weighted_map');
+    %         colormap gray;
+    %         set(gca,'YDir','normal');
+        end
+    end
+    samples(isnan([samples(:).pred_region])) = [];
+    accuracy = sum([samples().next_region] == [samples(:).pred_region])/length(samples);
+end
